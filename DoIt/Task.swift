d15909cd3f7fa0e43e86ee9cf4beb0f2eed3f1f2
ely-barros <elyb@Ely-MacBook-Pro.local>
2018-11-14 09:58:00 -0200

    Do It class 2

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Ely Barros on 12/11/18.
+//  Copyright © 2018 Ely. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
